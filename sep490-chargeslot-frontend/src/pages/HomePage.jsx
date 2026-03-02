41ff7d1162433831bf06f7b084d652f3c52df2ef
Đinh Thái Huy <huydthe171205@fpt.edu.vn>
2026-03-03 03:35:17 +0700

    update homepage

--- sep490-chargeslot-frontend/src/pages/HomePage.jsx
+++ sep490-chargeslot-frontend/src/pages/HomePage.jsx
@@ -6,11 +6,11 @@ export default function HomePage() {
     <div className="pt-20">
       <div className="min-h-60 flex flex-col items-center justify-center gap-5">
         <h1 className="text-3xl font-bold">Đặt lịch sạc xe điện dễ dàng</h1>
-        <p>
+        <p className="text-xl">
           Dễ dàng đặt trước lịch sạc cho xe điện của bạn với ChargeSlot. Trải
           nghiệm sự tiện lợi và tiết kiệm thời gian
         </p>
-        <Button className="w-40 bg-orange-500 rounded-full text-md hover:bg-green-500 cursor-pointer">
+        <Button className="w-40 h-11 bg-orange-500 rounded-full text-xl hover:bg-green-500 cursor-pointer">
           Tìm trạm sạc
         </Button>
       </div>
