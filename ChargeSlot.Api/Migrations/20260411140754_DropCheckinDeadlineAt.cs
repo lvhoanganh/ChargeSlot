d402ea987b4d3e1739232387f9de25abcca38de5
lvhoanganh <laivuhoanganh.fj@gmail.com>
2026-04-11 21:53:58 +0700

    fix: remove seed data changes from migration to prevent FK conflict on deploy

--- ChargeSlot.Api/Migrations/20260411140754_DropCheckinDeadlineAt.cs
+++ ChargeSlot.Api/Migrations/20260411140754_DropCheckinDeadlineAt.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -11,39 +11,19 @@ namespace ChargeSlot.Api.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DeleteData(
-                table: "Wallet",
-                keyColumn: "Id",
-                keyValue: 4);
-
             migrationBuilder.DropColumn(
                 name: "CheckinDeadlineAt",
                 table: "Booking");
-
-            migrationBuilder.InsertData(
-                table: "Wallet",
-                columns: new[] { "Id", "AvailableBalance", "CreatedAt", "FrozenBalance", "SystemCode", "UserId", "WalletType" },
-                values: new object[] { 99, 0m, new DateTime(2026, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), 0m, "TAX_HOLD", null, "System" });
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DeleteData(
-                table: "Wallet",
-                keyColumn: "Id",
-                keyValue: 99);
-
             migrationBuilder.AddColumn<DateTime>(
                 name: "CheckinDeadlineAt",
                 table: "Booking",
                 type: "datetime2",
                 nullable: true);
-
-            migrationBuilder.InsertData(
-                table: "Wallet",
-                columns: new[] { "Id", "AvailableBalance", "CreatedAt", "FrozenBalance", "SystemCode", "UserId", "WalletType" },
-                values: new object[] { 4, 0m, new DateTime(2026, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), 0m, "TAX_HOLD", null, "System" });
         }
     }
 }
